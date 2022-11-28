ba196f645f4928b2d34666b4d9cd6a954a8783e2
mikepodolskiy <mikepodolskiy@gmail.com>
2022-11-28 20:51:39 +0300

    ubuntu made version - comments according to PEP8 added

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 
 from flask import Flask
 
+from utils import get_all, get_by_pk, get_by_skill
 
-from utils import load_candidates, get_all, get_by_pk, get_by_skill
-
-
-# variables
+# variables definition
 source = "candidates.json"
 
-
+# starting app
 app = Flask(__name__)
+
 
 @app.route("/")
 def main_page():
+    """
+    routing for main page, showing all candidates in required format
+    :return: all candidates data in required format
+    """
     candidates = get_all(source)
     output = "<br>"
     for person in candidates:
@@ -23,8 +26,14 @@ def main_page():
         output += "<br>"
     return f"<pre> {output} <pre>"
 
+
 @app.route("/candidates/<int:pk>")
 def get_candidate(pk):
+    """
+    routing for candidates pages, showing more information about required person
+    :param pk: single person pk
+    :return: person's data in required format
+    """
     person = get_by_pk(pk, source)
     if not person:
         return "Candidate not found"
@@ -39,12 +48,17 @@ def get_candidate(pk):
     output += "<br>"
     return f"<pre> {output} <pre>"
 
+
 @app.route("/candidates/skills/<skill>")
 def get_candidate_by_skill(skill):
+    """
+    routing for skill page, showing list of persons having required skill
+    :param skill: required skill
+    :return: persons having required skill in required format
+    """
     candidates = get_by_skill(skill, source)
     output = "<br>"
     for person in candidates:
-
         output += person["name"] + "<br>"
         output += person["position"] + "<br>"
         output += person["gender"] + "<br>"
@@ -54,4 +68,5 @@ def get_candidate_by_skill(skill):
     return f"<pre> {output} <pre>"
 
 
+# launching app
 app.run()
--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ import requests
 
 def load_candidates(source):
     """
-    function imports data from weg or json file
-    :param source: source of data - url or filename
+    function imports data from web or json file
+    :param source: filename or url with json data
     :return: data from file when detecting ".json" in name, or trying to get data from web in another case
     """
     if ".json" in source:
@@ -18,12 +18,22 @@ def load_candidates(source):
         return response.json()
 
 
-
 def get_all(source):
+    """
+    get all candidates from source
+    :param source: filename or url with json data
+    :return: load_candidates function value
+    """
     return load_candidates(source)
 
 
 def get_by_pk(pk, source):
+    """
+    function finds one candidate from data according its primary key
+    :param pk: given primary key
+    :param source: filename or url with json data
+    :return: data of person having given pk (dict)
+    """
     candidates = load_candidates(source)
     for person in candidates:
         if person["pk"] == pk:
@@ -31,13 +41,15 @@ def get_by_pk(pk, source):
 
 
 def get_by_skill(skill_name, source):
-    # names_by_skills = []
-    # for person in candidates:
-    #     if skill_name.lower() in person["skills"].lower():
-    #         names_by_skills.append(person["name"])
-    # return ", ".join(names_by_skills)
+    """
+    function searching for persons with required skill
+    :param skill_name: required skill
+    :param source: filename or url with json data
+    :return: list of persons with required skill
+    """
     candidates = load_candidates(source)
     return [person for person in candidates if skill_name.lower() in person["skills"].lower().split(", ")]
 
+
+print(get_by_pk(3, "candidates.json"))
 print(get_by_skill("python", "candidates.json"))
-print("делегирование, пользоваться календарем, обсуждать важные вопросы".split(", "))
